ea48c3a2d3470a7d2f7ed9182c1b1fa6a811d6f2
romankt21 <romankt21@gmail.com>
2022-11-24 20:50:19 +0200

    Initial commit

---
+++ first/test.py
@@ -0,0 +1,22 @@
+import os
+
+
+# print(os.environ)
+# print(os.getcwd())
+
+# print(os.listdir())     # покаже файли і папки на  1 рівні в списку   ['.git', '.idea', 'test.py']
+
+# os.mkdir('first')       # створює папку
+# os.mkdir('second')
+
+# print(os.path.join('folder', '222', '1.txt'))    # join прирівнює / або\ в різних ОС рез folder\222\1.txt.
+                                                # # Повертає слеші відповідно до ОС
+
+# os.rename('test.py', 'test2.py')               # перейменувати файл з test.py на test2.py  або перенести файл
+
+# os.rename('test2.py',  os.path.join('first', 'test.py'))      # файл test2.py перенести файл в папку first і
+                                                            # перейменувати на test.py
+
+
+for item in os.walk('.'):                                           # перебирання папок
+    print(item)
